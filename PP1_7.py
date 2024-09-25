10bb72fd656dad2be0f9d3c6b3b82ea097da5abc
Mansoor-Muhammad-ICD2O1-01 <mansoor.muhammad@student.tdsb.on.ca>
2024-09-25 22:38:58 +0000

    1.7

--- PP1_7.py
+++ PP1_7.py
@@ -1,19 +1,46 @@
+'''
+Lesson: 1.7 Booleans
+'''
 
 
-q1():
-  #Write code here
 
-q2():
-  #Write code here
+def q1():
+  bool1 = True
+  print(bool1)
 
-q3():
-  #Write code here
 
-q4():
-  #Write code here
+def q2():
+  wendy = input("type an integer: ")
+  wendy = int(wendy)
+  bool1 = wendy > 5
+  print(bool1)
 
-q5():
-  #Write code here
+def q3():
+  weak = input("Input the letter a: ")
+  weak = float(weak)
+  a = int(weak)
+  bool1 = True
+  bool1 = weak == a
+  print(bool1)
+
+
+def q4():
+  weird = input("Input a word earlier in the dictionary than google: ")
+  weird = int(weird)
+  bool1 = True
+  bool1 = weird > "google" 
+  print(bool1)
+
+def q5():
+  sean = input("Input an integer: ")
+  sean = int(sean)
+  den = input("Input another integer: ")
+  den = int(den)
+  bool1 = True
+  
+  print("your numbers multiplied together are greater than 40:  ")
+
+  bool1 = {den + sean} > 40
 
 #Do edit the code below
 #Comment the lines below when running your tests
